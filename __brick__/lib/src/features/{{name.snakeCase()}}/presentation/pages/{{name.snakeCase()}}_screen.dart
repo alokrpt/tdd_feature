af4b989fc8ceace8da1ec7f2a7e36a55cf12e567
alokrpt <alokrpt@gmail.com>
2022-06-28 17:20:32 +0530

    screen

--- __brick__/lib/src/features/{{name.snakeCase()}}/presentation/pages/{{name.snakeCase()}}_screen.dart
+++ __brick__/lib/src/features/{{name.snakeCase()}}/presentation/pages/{{name.snakeCase()}}_screen.dart
@@ -1,10 +1,50 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
+import '../../../../core/extensions/context_extension.dart';
+import '../../../../core/injector/injection_container.dart';
+import '../../../../presentation/atoms/loader.dart';
+import '../../../../presentation/custom_widgets/custom_sliver_appbar.dart';
+import '../../../../presentation/molecules/failure_view.dart';
+import '../cubit/{{name.snakeCase()}}_cubit.dart';
 class {{name.pascalCase()}}Screen extends StatelessWidget {
   const {{name.pascalCase()}}Screen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return BlocProvider(
+      create: (context) =>
+          {{name.pascalCase()}}Cubit(sl())..{{name.camelCase()}}(id: "demoId"),
+      child: Scaffold(
+        body: BlocBuilder<{{name.pascalCase()}}Cubit, {{name.pascalCase()}}State>(
+          builder: (context, state) {
+            return CustomScrollView(
+              slivers: [
+                CustomSliverAppBar(
+                  title: "{{name.sentenceCase()}}",
+                ),
+                if (state is {{name.pascalCase()}}Loading)
+                  sliverCenterLoader
+                else if (state is {{name.pascalCase()}}Failed)
+                  FailureView(
+                    useSliver: true,
+                    type: state.type,
+                  )
+                else if (state is {{name.pascalCase()}}Loaded)
+                  SliverToBoxAdapter(
+                    child: Column(
+                      children: [
+                        // TODO: Insert your own widgets.
+                      ]
+                    ),
+                  )
+                else
+                  const Text('Unexpected State')
+              ],
+            );
+          },
+        ),
+      ),
+    );
   }
 }
